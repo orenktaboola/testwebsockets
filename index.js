3a9bdb01778a254dd7b61e071f47034e47758f66
Oren K <27916995+orenktaboola@users.noreply.github.com>
2018-12-02 16:43:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,14 +17,16 @@ const io = socketIO(server);
 
 // Register "connection" events to the WebSocket
 io.on("connection", function(socket) {
+  io.emit('connected to my app');
+
   // Register "join" events, requested by a connected client
-  socket.on("join", function (room) {
-    // join channel provided by client
-    socket.join(room)
-    // Register "image" events, sent by the client
-    socket.on("image", function(msg) {
-      // Broadcast the "image" event to all other clients in the room
-      socket.broadcast.to(room).emit("image", msg);
-    });
-  })
+  // socket.on("join", function (room) {
+  //   // join channel provided by client
+  //   socket.join(room)
+  //   // Register "image" events, sent by the client
+  //   socket.on("image", function(msg) {
+  //     // Broadcast the "image" event to all other clients in the room
+  //     socket.broadcast.to(room).emit("image", msg);
+  //   });
+  // })
 });
